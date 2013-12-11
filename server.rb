bb904ffc69a3635524d1912f53ab82e69d9b3e96
anath <anathabensour@gmail.com>
2013-12-11 13:00:41 +0000

    new design

--- server.rb
+++ server.rb
@@ -17,17 +17,30 @@ def puzzle(sudoku)
 end
 
 
-get '/solution' do 
-	@current_solution = session[:solution]
+get '/' do
+	prepare_to_check_solution
+	generate_new_puzzle_if_necessary
+	@current_solution = session[:current_solution] || session[:puzzle]
+	@solution = session[:solution]
+	@puzzle = session[:puzzle]
 	erb :index
 end
 
-post '/' do
-	cells = box_order_to_row(params["cell"])
-	session[:current_solution] = cells.maps{|value| value_to.i}.join
-	session[:check] = true
-	redirect to("/")
+
+get '/solution' do
+  @current_solution = session[:solution]
+  erb :index
 end
+
+post '/' do
+  cells = box_order_to_row_order(params["cell"])  
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
+end
+
+
+
 
 def box_order_to_row(cells)
 	boxes = cells.reach_slice(9).to_a
@@ -42,6 +55,58 @@ def box_order_to_row(cells)
 			memo += three_rows_of_the_three.flatten
 		}
 end
+
+def generate_new_puzzle_if_necessary
+	return if session[:current_solution]
+	sudoku = random_sudoku
+	session[:solution] = sudoku
+	session[:puzzle] = puzzle(sudoku)
+	session[:current_solution] = session[:puzzle]
+end
+
+def prepare_to_check_solution
+	@check_solution = session[:check_solution]
+	session[:check_solution] = nil
+end
+
+helpers do
+  def cell_value(value)
+    value.to_i == 0 ? '' : value
+  end
+
+  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+    must_be_guessed = puzzle_value == 0
+    tried_to_guess = current_solution_value.to_i != 0
+    guessed_incorrectly = current_solution_value != solution_value
+
+    if solution_to_check && 
+        must_be_guessed && 
+        tried_to_guess && 
+        guessed_incorrectly
+      'incorrect'
+    elsif !must_be_guessed
+      'value-provided'
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
